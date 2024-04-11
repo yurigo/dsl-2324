18afa25da89cca883aa1180fb3d419c96916aada
yuri@laSalle <yuri.gonzalez@salleurl.edu>
2024-04-11 17:04:19 +0200

    add middlewares to users

--- ejemplos/todos v3.3/router/users.route.js
+++ ejemplos/todos v3.3/router/users.route.js
@@ -1,7 +1,7 @@
 // voy a considerar que estoy en: /todos
 
 import express from "express";
-const router = express.Router()
+const router = express.Router({})
 
 import { allUsersController,
   getUsersController, 
@@ -12,10 +12,32 @@ import { allUsersController,
 
 import routeTodos from "./users/todos.route.js";
 
-function a(req,res,next){
-  console.log("a");
-  res.send("a");
+function middlewareUsers( req , res , next ){
+  console.log(req.query.pepito);
+  const pepito = Number(req.query.pepito)
+  if (pepito >= 10) {
+    next();
+  }
+  else{
+    next({
+      status: 404,
+      msg: "Pepito es inferios a 10"
+    })
+  }
 }
+
+function middlewareGenericErrorLogging(err,  req , res , next ){
+  console.log("Error: " + err.msg + "!!!");
+  next(err);
+}
+
+function middlewareGenericError(err,  req , res , next ){
+  res.status(err.status).json({error: "Ups! algo ha pasado: " + err.msg})
+}
+
+router.use(middlewareUsers)
+router.use(middlewareGenericErrorLogging)
+router.use(middlewareGenericError)
 
 router.get("/", allUsersController);
 router.get("/:id", getUsersController);
